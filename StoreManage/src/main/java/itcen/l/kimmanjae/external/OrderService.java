a5aa94b84f71a80046bffafe710d197dbb9bbf2a
KimMan-jae <47963629+KimMan-jae@users.noreply.github.com>
2022-12-09 10:50:25 +0900

    Update OrderService.java

--- StoreManage/src/main/java/itcen/l/kimmanjae/external/OrderService.java
+++ StoreManage/src/main/java/itcen/l/kimmanjae/external/OrderService.java
@@ -5,12 +5,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
-@FeignClient(name = "Order", url = "${api.url.Order}")
+@FeignClient(name = "Order", url = "${api.url.Order}", fallback = OrderServiceFallback.class)
 public interface OrderService {
     @RequestMapping(method= RequestMethod.GET, path="/orders/{id}")
-    public Order getOrder(@PathVariable("id") String id);
+    public Order getOrder(@PathVariable("id") String id, Integer qty);
 }
 
+@Service
+public class OrderServiceFallback implements OrderService{
+    public Order getOrder(String id, Integer qty){
+        Order fallbackValue = new Order();
+        fallbackValue.setQty(-qty);
+
+        return fallbackValue;
+    }
+}
